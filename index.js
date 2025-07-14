cdaf2414e237f3c9ccf38f77f1aa988c547aaa38
Axcel Joh H. Bernal <148522639+AJ-programmer@users.noreply.github.com>
2025-07-14 15:29:49 +0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,22 @@
+function add(firstNumber, secondNumber){
+    return firstNumber + secondNumber;
+}
+
+function subtract(firstNumber, secondNumber){
+    return firstNumber - secondNumber;
+}
+
+function multiply(firstNumber, secondNumber){
+    return firstNumber * secondNumber;
+}
+
+function divide(firstNumber, secondNumber){
+    return firstNumber / secondNumber;
+}
+
+console.log(add(20, 30));
+console.log(subtract(20, 30));
+console.log(multiply(20, 30));
+console.log(divide(20, 30));
+
+
